61b8d23fc6ff82a6da123c837246cc57163f1d3b
jlimtian <jl863@njit.edu>
2019-12-13 15:11:20 -0500

    initial commit

---
+++ project/login/login.php
@@ -0,0 +1,63 @@
+<?php
+    include("../signin.php");
+    valLogin();
+?>
+
+<!DOCTYPE html>
+<html lang="em">
+
+<head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+        shrink-to-fit=no">
+    <!-- main.css -->
+    <link rel="stylesheet" href="">
+
+    <!-- main.js -->
+    <script src=""></script>
+
+    <!-- Favicon and Tab Head -->
+    <link rel='icon' href='../images/favicon.png' type='image/x-icon'/>
+    <title>Sign In | NAME</title>
+</head>
+
+<body>
+    <header>
+	    <div class="header" id="myHeader">
+            <h1>NAME</h1>
+        </div>
+    </header>
+
+    <main>
+	<!-- Background Image -->
+        <!-- Sign In Form -->
+        <div class="signin" id="mySignIn">
+            <form action="../signin.php" method="POST">
+            <h3>Sign In</h3><br><br>
+                <div class="form" id="myForm">
+                    <!-- Login Email -->
+                    <label for="email">Email</label><br>
+                    <input type="email" id="email" placeholder="myemail@email.com" name="email"
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required><br><br>
+                    <!-- Password -->
+                    <label for="password">Password</label><br>
+                    <input type="password" id="password" placeholder="mypassword" name="password" 
+                    required><br><br>
+                    <!-- Sign In -->
+                    <input type="submit" class="submit" name="signIn" value="Sign In"><br><br>
+                </div>
+            </form>
+        </div>
+    </main>
+    
+</body>
+
+<footer>
+        <div class="footer" id="myFooter">
+                <!-- <span class="text-muted">&copy; OUR NAMES, 2019 |  
+                    Terms Of Use  |  Privacy Statement</span> -->
+        </div>
+</footer>
+
+</html>
---
+++ project/login/signin.php
@@ -0,0 +1,35 @@
+<?php
+//error checking
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+function valLogin() {
+    
+        if(isset($_POST['email']) && isset($_POST['password'])) {        
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        require("config.php");
+            
+        // connect to database
+        $conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
+        $db = new PDO($conn_string, $username, $password);
+        $select_query = "select password, id from `accounts` where email=:username";
+        $stmt = $db->prepare($select_query);
+        $stmt->bindParam(':username', $email);
+        $stmt->execute();
+        $response = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // check passwords
+        if(password_verify($_POST['password'] , $response['password'])) {
+        // Put target page here
+        // header('TARGET PAGE');
+        }
+        else {
+            echo 'Incorrect Login';
+        }    
+    }
+}
+
+valLogin();
+?>
