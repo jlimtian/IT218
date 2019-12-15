c981e2888667340555aa80d8ef2888dd55130847
jlimtian <jl863@njit.edu>
2019-12-14 21:51:50 -0500

    logout page made

---
+++ project/login/logout.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="em">
+
+<head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+        shrink-to-fit=no">
+    <!-- main.css -->
+    <link rel="stylesheet" href="logout.css">
+
+    <!-- Favicon and Tab Head -->
+    <title>Logout | Just Do It</title>
+</head>
+
+<body>
+    <header>
+            <div class="header" id="myHeader">
+            <h1>Just Do It</h1>
+        </div>
+    </header>
+
+    <main>
+	<img src="background.jpg" alt="Background">
+        <form>
+		<h3>You have been logged out.</h3><br><br>
+        </form>
+    </main>
+
+</body>
+
+<footer>
+        <div class="footer" id="myFooter">
+                <span class="text-muted">&copy; OUR NAMES, 2019 |
+                    Terms Of Use  |  Privacy Statement </span>
+        </div>
+</footer>
+
+</html>
