c9387228c7420b61b67fbef27718f776e93677aa
jag94 <48959145+jag94@users.noreply.github.com>
2019-12-14 20:51:57 -0500

    Update create.php

--- project/login/create.php
+++ project/login/create.php
@@ -1,15 +1,13 @@
 <?php
+	$email = $_POST['mid'];
 	require 'db.php' ;
 	
 	$sql = "SELECT id, owneremail, ownerid FROM todos WHERE owneremail = :email";
-	$email = "janedoe@njit.edu"; //fix
+	
 	$db = new connection_db();
 	$conn = $db->connectDB();
 	$run_q = new run_SQL();
 	$returns = $run_q->runQuery($sql, $conn, $email);
-	
-	//$email = "janedoe@njit.edu"; change to $_POST
-	
 
 	$timedue = $_POST["timedue"];
 	$title = $_POST["title"];
@@ -21,11 +19,9 @@
 		$ownID = $return['ownerid'];
 	}
 	
-	//echo $idVal . " " . $ownID;
-	
 	$sql = "INSERT INTO todos VALUES (:idVal, :email, :ownID, :title, NOW(), :datedue, :message, 0)";
 	
-	//$sql = "INSERT INTO todos VALUES (8, :email, 10, :title, NOW(), '2019-11-08 01:02:00', :message, 0)";
-	
 	$returns = $run_q->runSQLQuery($sql, $conn, $idVal, $email, $ownID, $title, $datedue, $note);
+	header("refresh: 0, url='show.php'");
+	exit();
 ?>
