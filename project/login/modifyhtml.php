f787129ef581f327e5ed6a482466fbce02d9ae48
jag94 <48959145+jag94@users.noreply.github.com>
2019-12-14 23:36:56 -0500

    Update modifyhtml.php

--- project/login/modifyhtml.php
+++ project/login/modifyhtml.php
@@ -7,6 +7,29 @@
 <?php
 	$idVal = $_POST['btnmod'];
 	
+	require 'db.php';
+	$sql = "SELECT * FROM todos WHERE id = :idVal";
+	$db = new connection_db();
+	$conn = $db->connectDB();
+	$run_q = new run_SQL();
+	$returns = $run_q->runDeleteQuery($sql, $conn, $idVal);
+	
+	
+	echo "<br><br><br><table border=\"0\"><tr><th>ID</th><th>Title</th><th>Date Created</th><th>Date Due</th><th>Message</th></tr>";
+	foreach($returns as $return) {
+?>
+
+<tr>
+	<td align="center"><?php echo $return['id']; ?></td>
+	<td align="center"><?php echo $return['title']; ?></td>
+	<td align="center"><?php echo $return['createddate']; ?></td>
+	<td align="center"><?php echo $return['duedate']; ?></td>
+	<td align="center"><?php echo $return['message']; ?></td>
+</tr>
+
+<?php 
+	echo "<br>"; }
+	
 	//logout on it
 ?>
 
@@ -16,9 +39,11 @@
 	<input type = "date" name = "datedue"> Enter New Due Date<br><br>
 	<input type = "time" name = "timedue"> Enter New Time Due<br><br>
 	<input type = text name = "title"> Enter New Title<br><br>
-
+	
 	<input type = text name = "description" max = 144> Enter New Description<br><br>
-	<input type="checkbox" name="complete" value="complete"> Task Complete?<br><br>
+	
+	<input type="checkbox" name="complete" value="on"> Task Complete?<br><br>
+	
 	<button type = "submit" name = "inter" value = "<?php echo $idVal;?>" formmethod = "post">Save</button>
 	
 </form>
