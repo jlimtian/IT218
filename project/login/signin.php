424007762aa5193431d7ba1123ca541e77269470
jag94 <48959145+jag94@users.noreply.github.com>
2019-12-15 01:38:44 -0500

    Update signin.php

--- project/login/signin.php
+++ project/login/signin.php
@@ -1,5 +1,6 @@
 <?php
 //error checking
+session_start();
 ini_set('display_errors',1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -13,23 +14,20 @@ function valLogin() {
 			
 	
         // connect to database
-        $sql = "SELECT id, password FROM accounts WHERE email = :email";
+        $sql = "SELECT id, password, email FROM accounts WHERE email = :email";
         $db = new connection_db();
 		$conn = $db->connectDB();
 		$run_q = new run_SQL();
         $responses = $run_q->runQuery($sql, $conn, $email);
 		foreach($responses as $response) {
 			$t_pass = $response['password'];
+			$t_email = $response['email'];
+			$id = $response["id"];
 		}
-		
-		$id = $response["id"];
-		
-		//echo $t_pass . "<br>";
-		//echo $password;
 
         // check passwords
-        if($password == $t_pass) {
-        // Put target page here
+        if(($password == $t_pass) and ($email == $t_email)) {
+        $_SESSION["id"] = $id;
         header("refresh: 0, url='show.php'");
         }
         else {
